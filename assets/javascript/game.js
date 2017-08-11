eda55d7dcad9cd29619d2baeb8a65687fb375254
gcdwyer <gcdwyer@gmail.com>
2017-08-10 23:59:08 -0400

    initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+
+
+// Variables ===========================================================
+
+console.log("start game");
+
+
+
+// click button and 
+$("#startButton").on("click", function() {
+
+	if ($("#radio3").is(":checked")) {
+
+   		console.log("3 is correct");
+
+	} else {
+		console.log("incorrect");
+	}
+
+});
+
+
+    
+
+
+
+// Start timer to count down to 0 when start button is pressed
+
+// Ask question
+
+// Radio butotn with 4 possible answers
+
+// Done button
+
+// Count correct answers
+
+// Count incorrect answers
+
+// count unanswered questions
