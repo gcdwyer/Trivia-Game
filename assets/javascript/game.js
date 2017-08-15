9b440a07b1bbeae11efe4830d6dbb0aa872f6fa6
gcdwyer <gcdwyer@gmail.com>
2017-08-15 01:56:53 -0400

    added results and started reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,71 @@
 window.onload = function() {
 
+	// click start button to start game
     $("#startButton").on("click", game.start);
+    // hide reset button until game is finished
+    $("#resetButton").hide();
 
 };
 
+// variables ============================================================================
 
-// variables //
 var questions = [
 
-{question:"How many miles is the sun from the Earth?",
-answers: [" 93 Billion "," 93 Million "," Neither "],
-correctAnswer: "93 Million"
+{question:"How many squares are in normal Monopoly board?",
+answers: [" 20 ", " 40 ", " 60 ", " 80 "],
+correctAnswer: " 40 "
 },
 
-{question: "How long has Jupiter's Red Spot been known to exist?",
-answers: ["300 years", "50 years", "500 years"],
-correctAnswer: "300 years"
+{question: "What is the largest planet of our Solar System called?",
+answers: [" Earth ", " Jupiter ", " Uranus ", " Saturn "],
+correctAnswer: " Jupiter "
 },
 
-{question: "Which planet is earth's closest twin in the Solar System?",
-answers: ["Venus", "Mars", "Mercury"],
-correctAnswer: "Venus"
+{question: "What is the fourth novel of George R.R. Martin's A Song of Ice and Fire book series?",
+answers: [" A Storm of Swords ", " A Clash of Kings ", " A Feast for Crows ", " A Dance with Dragons "],
+correctAnswer: " A Feast for Crows "
 },
 
-{question:"Which planet is the Sun's closets twin in our Solar System?",
-answers: ["Neptune", "Jupiter", "Venus"],
-correctAnswer: "Jupiter"
+{question:"What percent of people live North of the equator?",
+answers: [" 60% ", " 70% ", " 80% ", " 90% "],
+correctAnswer: " 90% "
 },
 
-{question:"What protects us from the Sun's charged particles?",
-answers: ["The moon", "Magnetic field", "Superman"],
-correctAnswer: "Magnetic field"
+{question:"How many times does a heart beats in a day?",
+answers: [" 1,000 ", " 10,000 ", " 100,000 ", " 1,000,000 "],
+correctAnswer: " 100,000 "
 
 }];
-
-
 
 var intervalId;
 
 var clockRunning = false;
 
-// game object ============================================================
+// object =============================================================================
 var game = {
 
 	correct: 0,
 	incorrect: 0,
-	timer: 8,
+	timer: 4,
 
 	start: function() {
+		// hides start button after game starts
+		$("#startButton").hide();
 
-		console.log("function start");
-
-    	// DONE: Use setInterval to start the count here and set the clock to running.
+    	// if clock is not running
 	    if (!clockRunning) {
-
+	    	// set counter interval to 1 second
 	        intervalId = setInterval(game.count, 1000);
-
+	        // set clocl to running
 	        clockRunning = true;
-
+	        // run loop through all questions
 	        for (var i = 0; i < questions.length; i++) {
-	        	
+	        	// display questions in html	
 	        	$(".questions").append("<h2> " + questions[i].question + " </h2>");
-
+	        	// run loop though all possible answers
 	        	for (var j = 0; j < questions[i].answers.length; j++) {
-
-		        	$(".questions").append("<input type = 'radio' name = 'question'" + i + " value=" + questions[i].answers[j] + "'>" + questions[i].answers[j])
+	        		// display answers in html as radio buttons
+		        	$(".questions").append("<input type= 'radio' name = 'question-"+i+"' value='"+questions[i].answers[j]+"'>"+questions[i].answers[j])
 		        }
 	    	}
 	    }
@@ -72,85 +73,121 @@ var game = {
 
     count: function() {
 
-    	console.log("function count");
-
-	    // DONE: increment time by 1, remember we cant use "this" here.
+	    // decrement counter by 1
 	    game.timer--;
 
-	    // DONE: Use the variable we just created to show the converted time in the "display" div.
+	    // display timer in html
 	    $(".timer").html("<h2>Time Remaining: " + game.timer + " seconds<h2>");
-
+	    // when timer reaches 0, finish game
 	    if (game.timer <= 0) {
-
-	    	console.log("game finish4ed");
-
+	    	console.log("game finished");
 	    	game.finish();
-
-	    }
+	    } 
 	},
 
-
-
 	finish: function() {
-
-		// // 	if ($("#radio3").is(":checked")) {
-
-		console.log("finish function");
-
-		$.each($('input[name="question0"]:checked'), function() {
-
-			if($(this).val()==questions[0].correctAnswer) {
-
+		// determine which radio button is checked
+		$.each($('input[name="question-0"]:checked'), function() {
+			// if correct answer, increment correct variable
+			if($(this).val()==questions[0].correctAnswer){
 				game.correct++;
-
+			// if incorrect answer, increment incorrect variable
 			} else {
-
 				game.incorrect++;
 			}
-
 		});
 
+		$.each($('input[name="question-1"]:checked'), function() {
+
+			if($(this).val()==questions[1].correctAnswer){
+				game.correct++;
+			} else {
+				game.incorrect++;
+			}
+		});
+
+		$.each($('input[name="question-2"]:checked'), function() {
+
+			if($(this).val()==questions[2].correctAnswer){
+				game.correct++;
+			} else {
+				game.incorrect++;
+			}
+		});
+
+		$.each($('input[name="question-3"]:checked'), function() {
+
+			if($(this).val()==questions[3].correctAnswer){
+				game.correct++;
+			} else {
+				game.incorrect++;
+			}
+		});
+
+		$.each($('input[name="question-4"]:checked'), function() {
+
+			if($(this).val()==questions[4].correctAnswer){
+				game.correct++;
+			} else {
+				game.incorrect++;
+			}
+		});
+		// jump to results after score is calculated
 		game.results();
 
 	},
 
 
 	results: function() {
-
+		// stop timer
 		clearInterval(intervalId);
+		// determine unanswered questions
+		var unanswered = questions.length - game.correct - game.incorrect
+		// hide timer and quesitons in html
+		$(".timer").hide();
+		$(".questions").hide();
 
-		$(".timer").remove();
-		$(".questions").remove();
-
-		$(".results").append("<h2>Correct: " + game.correct + "</h2>");
+		// display results in html
+		$(".results").html("<h2>Correct: " + game.correct + "</h2>");
 		$(".results").append("<h2>Incorrect: " + game.incorrect + "</h2>");
+		$(".results").append("<h2>Unanswered: " + unanswered + "</h2>");
+		// show reset button
+		$("#resetButton").show();
 
-
-
-
-
+		game.reset();
 	},
 
 
+	// still need to add reset button after results displayed
+	reset: function () {
+		// resetting variables
+		game.correct = 0;
+		game.incorrect = 0;
+		timer = 3;
+		clockRunning = false;
+
+		console.log("got to reset before press");
+
+		$("#resetButton").on("click", function() {
+
+			if (clockRunning == false) {
+				// removes the results from previous game
+				$(".results h2").remove();
+				// hides reset button after its clicked
+				$("#resetButton").hide();
+				// shows timer and question divs
+				$(".timer").show();
+				$(".questions").show();
+				// removes radio button selections
+				$('input[name="question-0"]').prop('checked', false);
+				$('input[name="question-1"]').prop('checked', false);
+				$('input[name="question-2"]').prop('checked', false);
+				$('input[name="question-3"]').prop('checked', false);
+				$('input[name="question-4"]').prop('checked', false);
+
+				// game.start();
+
+			}
+		});
+	},
 };
-
-
-	
-
-
-
-
-
-// Start timer to count down to 0 when start button is pressed
-
-// Ask question
-
-// Radio butotn with 4 possible answers
-
-// Done button
-
-// Count correct answers
-
-// Count incorrect answers
-
-// count unanswered questions
